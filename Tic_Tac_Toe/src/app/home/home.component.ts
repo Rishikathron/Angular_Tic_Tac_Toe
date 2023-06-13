b0ff9e3f515efa503410cb018da5a0fed06630cb
RAVI <rishwanth.ravi@accenture.com>
2023-06-13 20:44:06 +0530

    Changes implemented with ui and backend (logic part for winning is  pending)

--- Tic_Tac_Toe/src/app/home/home.component.ts
+++ Tic_Tac_Toe/src/app/home/home.component.ts
@@ -7,27 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  Playerx_Arr : number[][] = [[0,0,0],[0,0,0],[0,0,0]];
+  Playero_Arr : number[][] = [[0,0,0],[0,0,0],[0,0,0]];
+
   playerx : boolean = false;
   playero : boolean = false;
+
   started : boolean = false;
-
-  onClick(){   
-    console.log("onclick called");
-    this.playerx = !this.playero;
-    this.playero = !this.playerx;
-    // if(this.playerx == false){
-      
-    //   }   
-    // if(this.playero == false)
-    // {
-    //   this.playero = true;
-    //   this.playerx = false;
-    // }
-  }
-
+  
   start(){
     if(this.started == false){
       this.started = true;
     }
   }
+
+  onClick(row : number, col : number ){ 
+    this.playerx = !this.playerx
+    if(this.playerx == true){
+      this.Playerx_Arr[row][col] = 1;
+    }
+    else{
+      this.Playero_Arr[row][col] = 1;
+    }
+    console.log(this.Playerx_Arr);
+    this.playero = !this.playerx;
+  }
+
+
 }
